8d22a747f48eff7b23329259f4a3347fc578e080
Sergii Simakhin <sergio@mrsergio.com>
2018-04-10 21:30:49 +0700

    Updated crop overlayer UI: corners become a bit far from image, image borders become bolder.

--- Objective-C/TOCropViewController/Views/TOCropOverlayView.m
+++ Objective-C/TOCropViewController/Views/TOCropOverlayView.m
@@ -22,7 +22,8 @@
 
 #import "TOCropOverlayView.h"
 
-static const CGFloat kTOCropOverLayerCornerWidth = 20.0f;
+static const CGFloat kTOCropOverLayerCornerWidth = 12.0f;
+static const CGFloat kTOCropOverLayerOutlineWidth = 2.0f;
 
 @interface TOCropOverlayView ()
 
@@ -93,10 +94,10 @@ static const CGFloat kTOCropOverLayerCornerWidth = 20.0f;
         
         CGRect frame = CGRectZero;
         switch (i) {
-            case 0: frame = (CGRect){0,-1.0f,boundsSize.width+2.0f, 1.0f}; break; //top
-            case 1: frame = (CGRect){boundsSize.width,0.0f,1.0f,boundsSize.height}; break; //right
-            case 2: frame = (CGRect){-1.0f,boundsSize.height,boundsSize.width+2.0f,1.0f}; break; //bottom
-            case 3: frame = (CGRect){-1.0f,0,1.0f,boundsSize.height+1.0f}; break; //left
+            case 0: frame = (CGRect){0,-kTOCropOverLayerOutlineWidth,boundsSize.width + kTOCropOverLayerOutlineWidth, kTOCropOverLayerOutlineWidth}; break; //top
+            case 1: frame = (CGRect){boundsSize.width,0.0f,kTOCropOverLayerOutlineWidth,boundsSize.height}; break; //right
+            case 2: frame = (CGRect){0,boundsSize.height,boundsSize.width + kTOCropOverLayerOutlineWidth,kTOCropOverLayerOutlineWidth}; break; //bottom
+            case 3: frame = (CGRect){0,0,kTOCropOverLayerOutlineWidth,boundsSize.height}; break; //left
         }
         
         lineView.frame = frame;
@@ -107,23 +108,23 @@ static const CGFloat kTOCropOverLayerCornerWidth = 20.0f;
     for (NSInteger i = 0; i < 4; i++) {
         NSArray *cornerLine = cornerLines[i];
         
-        CGRect verticalFrame = CGRectZero, horizontalFrame = CGRectZero;
+        CGRect verticalFrame = CGRectZero, horizontalFrame = CGRectZero; 
         switch (i) {
             case 0: //top left
-                verticalFrame = (CGRect){-3.0f,-3.0f,3.0f,kTOCropOverLayerCornerWidth+3.0f};
-                horizontalFrame = (CGRect){0,-3.0f,kTOCropOverLayerCornerWidth,3.0f};
+                verticalFrame = (CGRect){-5.0f,-5.0f - kTOCropOverLayerOutlineWidth,3.0f,kTOCropOverLayerCornerWidth+2.0f};
+                horizontalFrame = (CGRect){-3.0f,-5.0f - kTOCropOverLayerOutlineWidth,kTOCropOverLayerCornerWidth,3.0f};
                 break;
             case 1: //top right
-                verticalFrame = (CGRect){boundsSize.width,-3.0f,3.0f,kTOCropOverLayerCornerWidth+3.0f};
-                horizontalFrame = (CGRect){boundsSize.width-kTOCropOverLayerCornerWidth,-3.0f,kTOCropOverLayerCornerWidth,3.0f};
+                verticalFrame = (CGRect){boundsSize.width + 4.0f,-5.0f - kTOCropOverLayerOutlineWidth,3.0f,kTOCropOverLayerCornerWidth+2.0f};
+                horizontalFrame = (CGRect){boundsSize.width-kTOCropOverLayerCornerWidth + 5.0f,-5.0f - kTOCropOverLayerOutlineWidth,kTOCropOverLayerCornerWidth,3.0f};
                 break;
             case 2: //bottom right
-                verticalFrame = (CGRect){boundsSize.width,boundsSize.height-kTOCropOverLayerCornerWidth,3.0f,kTOCropOverLayerCornerWidth+3.0f};
-                horizontalFrame = (CGRect){boundsSize.width-kTOCropOverLayerCornerWidth,boundsSize.height,kTOCropOverLayerCornerWidth,3.0f};
+                verticalFrame = (CGRect){boundsSize.width + 4.0f,boundsSize.height-kTOCropOverLayerCornerWidth + 4.0f,3.0f,kTOCropOverLayerCornerWidth+3.0f};
+                horizontalFrame = (CGRect){boundsSize.width-kTOCropOverLayerCornerWidth + 5.0f,boundsSize.height + 4.0f,kTOCropOverLayerCornerWidth,3.0f};
                 break;
             case 3: //bottom left
-                verticalFrame = (CGRect){-3.0f,boundsSize.height-kTOCropOverLayerCornerWidth,3.0f,kTOCropOverLayerCornerWidth};
-                horizontalFrame = (CGRect){-3.0f,boundsSize.height,kTOCropOverLayerCornerWidth+3.0f,3.0f};
+                verticalFrame = (CGRect){-5.0f,boundsSize.height-kTOCropOverLayerCornerWidth + 4.0f,3.0f,kTOCropOverLayerCornerWidth};
+                horizontalFrame = (CGRect){-5.0f,boundsSize.height + 4.0f,kTOCropOverLayerCornerWidth+2.0f,3.0f};
                 break;
         }
         
